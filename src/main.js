e45251ee137ae92343e8a787066f2e742b657858
BuildTools <almogsurizon@gmail.com>
2019-12-26 16:30:36 +0200

    Created the Sorti project box. Change the theme to dark.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@ const gitHubBtn = document.querySelector("#gitHubBtn");
 gitHubBtn.addEventListener("click", (e) => {
     window.location.href = "https://github.com/AlmoGSIntel"
 })
+
+const InfoBoxes = document.querySelectorAll('.Info');
+
+
+InfoBoxes.forEach((info)=>{
+    info.parentElement.addEventListener("mouseenter", (e) => {
+        info.classList.add('moveUp');
+    });
+    info.parentElement.addEventListener("mouseleave", (e) => {
+        info.classList.remove('moveUp');
+
+    });
+
+})
